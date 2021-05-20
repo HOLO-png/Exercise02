dd60728ac26ec36c37b696cc2449c5a51659cde3
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2021-05-20 11:57:33 +0700

    first commit

---
+++ bai10.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+let chewieRegex = /t[a-z]*i/; // Change this line
+// Only change code above this line
+let chewieQuote = 'titanic';
+let result = chewieQuote.match(chewieRegex);
+console.log(result);
